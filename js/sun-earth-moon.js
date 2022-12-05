cface7da1539a118d718adf42b1e29beee4f6875
anna-marie-iqualify <anna-maire@iqualify.com>
2022-12-05 14:17:11 +1300

    making the sun

--- js/sun-earth-moon.js
+++ js/sun-earth-moon.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   function makeCamera() {
     const fov = 40;
-    const aspect = 2;  // the canvas default
+    const aspect = 2; // the canvas default
     const near = 0.1;
     const far = 1000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
@@ -9,11 +9,18 @@ $(document).ready(function () {
     return camera;
   }
 
-  function makeLight() {
+  function makeDirectionalLight() {
     const color = 0xffffff;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
+    return light;
+  }
+
+  function makePointLight() {
+    const color = 0xffffff;
+    const intensity = 3;
+    const light = new THREE.PointLight(color, intensity);
     return light;
   }
 
@@ -29,19 +36,47 @@ $(document).ready(function () {
     return needResize;
   }
 
+  // characters
+  function makeSphere(radius, segments) {
+    const widthSegments = segments;
+    const heightSegments = segments;
+    const sphereGeometry = new THREE.SphereBufferGeometry(
+      radius,
+      widthSegments,
+      heightSegments
+    );
+    return sphereGeometry;
+  }
+
   function main() {
     const canvas = $("#sun-earth-moon").get(0);
     const renderer = new THREE.WebGLRenderer({ canvas });
 
     const camera = makeCamera();
+    camera.position.set(0, 50, 0);
+    camera.up.set(0, 0, 1);
+    camera.lookAt(0, 0, 0);
 
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xAAAAAA);
+    scene.background = new THREE.Color(0xaaaaaa);
 
-    const light = makeLight();
+    const light = makeDirectionalLight();
 
     scene.add(light);
 
+    // Adding characters
+    // making the sun
+    const objects = [];
+    const sunShape = makeSphere(1, 6);
+    const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xffff00 });
+    const sunMesh = new THREE.Mesh(sunShape, sunMaterial);
+
+    sunMesh.scale.set(5, 5, 5); // make the sun large
+    scene.add(sunMesh);
+    objects.push(sunMesh);
+
+    const sunlight = makePointLight();
+    scene.add(sunlight);
 
     function animate(time) {
       time *= 0.001; // convert time to seconds
@@ -53,6 +88,9 @@ $(document).ready(function () {
         camera.updateProjectionMatrix();
       }
 
+      objects.forEach((obj) => {
+        obj.rotation.y = time;
+      });
 
       renderer.render(scene, camera);
 
